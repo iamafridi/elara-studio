04a41e95ef157598873ab20de91ed5cf02990518
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2024-02-23 22:06:06 +0600

    Footer initial done

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
+  el: document.querySelector("#main"),
+  smooth: true,
+});
+
+var elemContainer = document.querySelector("#elem-container");
+var fixedImage = document.querySelector("#fixed-image");
+elemContainer.addEventListener("mouseenter", function () {
+  fixedImage.style.display = "block";
+});
+elemContainer.addEventListener("mouseleave", function () {
+  fixedImage.style.display = "none";
+});
+
+var elems = document.querySelectorAll(".elem");
+elems.forEach(function (e) {
+  e.addEventListener("mouseenter", function () {
+    var Image = e.getAttribute("data-image");
+    fixedImage.style.backgroundImage = `url(${Image})`;
+  });
 });
