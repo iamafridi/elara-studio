771a7c26773e2cf13382d496a63440d9ccb9cb20
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2024-11-09 01:47:38 +0600

    Updated

--- script.js
+++ script.js
@@ -3,19 +3,33 @@ const scroll = new LocomotiveScroll({
   smooth: true,
 });
 
-var elemContainer = document.querySelector("#elem-container");
-var fixedImage = document.querySelector("#fixed-image");
-elemContainer.addEventListener("mouseenter", function () {
-  fixedImage.style.display = "block";
-});
-elemContainer.addEventListener("mouseleave", function () {
-  fixedImage.style.display = "none";
-});
-
-var elems = document.querySelectorAll(".elem");
-elems.forEach(function (e) {
-  e.addEventListener("mouseenter", function () {
-    var Image = e.getAttribute("data-image");
-    fixedImage.style.backgroundImage = `url(${Image})`;
+function page4Animation() {
+  var elemContainer = document.querySelector("#elem-container");
+  var fixedImage = document.querySelector("#fixed-image");
+  elemContainer.addEventListener("mouseenter", function () {
+    fixedImage.style.display = "block";
   });
-});
+  elemContainer.addEventListener("mouseleave", function () {
+    fixedImage.style.display = "none";
+  });
+
+  var elems = document.querySelectorAll(".elem");
+  elems.forEach(function (e) {
+    e.addEventListener("mouseenter", function () {
+      var Image = e.getAttribute("data-image");
+      fixedImage.style.backgroundImage = `url(${Image})`;
+    });
+  });
+}
+
+// Swiper
+function swiperAnimation() {
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 50,
+   
+  });
+}
+swiperAnimation();
+page4Animation();
