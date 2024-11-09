aad9fdcc5ca8b35bc48b9ffa11650493cc343c84
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2024-11-09 13:17:14 +0600

    Loading and minor update

--- script.js
+++ script.js
@@ -31,5 +31,34 @@ function swiperAnimation() {
    
   });
 }
-swiperAnimation();
-page4Animation();
+
+function menuAnimation() {
+
+  var menu = document.querySelector("nav h3")
+  var full = document.querySelector("#full-scr")
+  var navimg = document.querySelector("nav h2")
+  var flag = 0
+  menu.addEventListener("click", function () {
+      if (flag == 0) {
+          full.style.top = 0
+          navimg.style.opacity = 0
+          flag = 1
+      } else {
+          full.style.top = "-100%"
+          navimg.style.opacity = 1
+          flag = 0
+      }
+  })
+}
+
+function loaderAnimation() {
+  var loader = document.querySelector("#loader")
+  setTimeout(function () {
+      loader.style.top = "-100%"
+  }, 4200)
+}
+
+swiperAnimation()
+page4Animation()
+menuAnimation()
+loaderAnimation()
